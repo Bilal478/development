a59dd7f13e7847ca5c66c5b452cb3ab58e075d7a
Bilal478 <478bilal@gmail.com>
2020-06-08 08:57:50 +0500

    login edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible" >
   <title>Accridetation System</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
