c19a20f32ee3e2881e0af51932350ce23f4e68c7
Bilal478 <478bilal@gmail.com>
2020-06-08 09:04:05 +0500

    login edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('https://superioraccreditation.herokuapp.com/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
   <link rel="stylesheet"  href="{{asset('https://superioraccreditation.herokuapp.com/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
@@ -27,7 +27,7 @@
   <link rel="stylesheet" href="{{asset('https://superioraccreditation.herokuapp.com/css/app.css')}}" >
 
   <!-- Google Font: Source Sans Pro -->
-  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed " >
 <div class="d-flex justify-content-center ">
@@ -121,6 +121,6 @@
 <!-- AdminLTE for demo purposes -->
 <script href="{{asset('dist/js/demo.js')}}" ></script>
 {{-- <script href="{{asset('js/app.js')}}" ></script> --}}
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('https://superioraccreditation.herokuapp.com/js/app.js') }}"></script>
 </body>
 </html>
