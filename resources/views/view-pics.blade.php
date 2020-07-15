a18d54a34a5c8d592f55a9695ae45510db330209
Bilal478 <478bilal@gmail.com>
2020-07-15 12:52:56 +0500

    d

--- resources/views/view-pics.blade.php
+++ resources/views/view-pics.blade.php
@@ -8,10 +8,10 @@
   
     <div class="row  pl-5 p-2">
         @foreach ($exampics as $exampic)
-        <img src='storage/exam/midd/2122580450.png' alt="Lights" width="200rem" height="100rem" download>
+       
       <div class="bg-danger m-4">
         <div class="img-rounded">
-          <a href="{{$exampic->path}}" target="_blank">
+          <a href="{{url($exampic->path)}}" target="_blank">
             <img src="{{$exampic->path}}" alt="Lights" width="200rem" height="100rem" download>
             <div class="caption mt-1 ">
                 <a class=" bg-dark p-1" href="{{$exampic->path}}" download>Donload</a>
