ec2899d5b8973c0574330abc5b45834baec8a42a
Bilal478 <478bilal@gmail.com>
2020-07-15 09:07:17 +0500

    image donload

--- resources/views/view-pics.blade.php
+++ resources/views/view-pics.blade.php
@@ -12,7 +12,7 @@
       <div class="bg-danger m-4">
         <div class="img-rounded">
           <a href="{{'/'.$exampic->path}}" target="_blank">
-            <img src="{{'/'.$exampic->path}}" alt="Lights" width="200rem" height="100rem">
+            <img src="{{'/'.$exampic->path}}" alt="Lights" width="200rem" height="100rem" download>
             <div class="caption mt-1 ">
                 <a class=" bg-dark p-1" href="{{'/'.$exampic->path}}" download>Donload</a>
             </div>
