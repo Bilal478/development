4f196b4ff32de24d794ff67a044e0559c98ee4cb
Bilal478 <478bilal@gmail.com>
2020-07-15 12:36:03 +0500

    d

--- resources/views/view-pics.blade.php
+++ resources/views/view-pics.blade.php
@@ -11,10 +11,10 @@
        
       <div class="bg-danger m-4">
         <div class="img-rounded">
-          <a href="{{'/'.$exampic->path}}" target="_blank">
-            <img src="{{'/'.$exampic->path}}" alt="Lights" width="200rem" height="100rem" download>
+          <a href="{{$exampic->path}}" target="_blank">
+            <img src="{{$exampic->path}}" alt="Lights" width="200rem" height="100rem" download>
             <div class="caption mt-1 ">
-                <a class=" bg-dark p-1" href="{{'/'.$exampic->path}}" download>Donload</a>
+                <a class=" bg-dark p-1" href="{{$exampic->path}}" download>Donload</a>
             </div>
           </a>
         </div>
